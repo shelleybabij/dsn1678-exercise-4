871f25c39b462fe26d4f1a9be889e30ed4406459
shelleybabij <babijs2@hotmail.com>
2014-10-09 10:45:56 -0400

    adding transformation

--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@ var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
 var $shuttleSection = $('.shuttle-section');
 var $shuttle = $('.shuttle');
+var $shipSection = $('.ship-section');
+var $ship = $('.ship');
+var $ship1 = $('.ship1');
+var $ship2 = $('.ship2');
+var $ship3 = $('.ship3');
+var $ship4 = $('.ship4');
 
 $win.on('scroll', function (){
 	var scrollPos = $win.scrollTop();
 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos 	/ 2 + 'px');
-
+	$shipSection.css('background-position', 'center ' + scrollPos / 3 + 'px');
 });
 
 $dipperSection.waypoint(function () {
@@ -20,6 +26,12 @@ $dipperSection.waypoint(function () {
 
 $shuttleSection.waypoint(function () {
 	$shuttle.addClass('js-shuttle-fade');
-
 }, { offset: '50%'});
 
+
+$shipSection.waypoint(function () {
+	$ship1.addClass('js-ship-fade-1');
+	$ship2.addClass('js-ship-fade-2');
+	$ship3.addClass('js-ship-fade-1');
+	$ship4.addClass('js-ship-fade-2');
+}, { offset: '50%' });
